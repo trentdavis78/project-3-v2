55dba4317f9cca37d8fae749bc27f8fdcbca1875
Trent Davis <trentdavisinc@gmail.com>
2019-05-23 11:46:37 -0500

    Initial articles page layout

--- blog/src/pages/articles.js
+++ blog/src/pages/articles.js
@@ -10,7 +10,7 @@ const Articles = ({ data }) => (
   <Layout>
     <SEO title="Articles" />
     <Section styleName={"bg-white"}>
-     <RowContainer color={"white"}>
+      <RowContainer color={"white"}>
         {/* <ul>
           {data.allStrapiArticle.edges.map(document => (
             <li key={document.node.id}>
@@ -25,31 +25,38 @@ const Articles = ({ data }) => (
         <div className="row">
           <div className="col l9">
             <div className="row">
-              <div className="col s12 m8 l8">
-                {data.allStrapiArticle.edges.map(document => (
-                <div key={document.node.id}>                  
-                  <Img fluid={document.node.image.childImageSharp.fluid}/>
-                  <h4>
-                    <Link to={`/articles/${document.node.id}`}>{document.node.title}</Link>
-                  </h4>
-                </div>
-              ))}
-            </div>
-            <div className="col s12 m4 l4">
-                {data.allStrapiArticle.edges.map(document => (
-                <div key={document.node.id}>                  
-                  <Img fluid={document.node.image.childImageSharp.fluid}/>
-                  <h6>
-                    <Link to={`/articles/${document.node.id}`}>{document.node.title}</Link>
-                  </h6>
-                </div>
-              ))}
-            </div>
+              {data.allStrapiArticle.edges.map((document, idx) => {
+                if (idx % 3 === 0) {
+                  return (
+                    <>
+                      <div className="col s12 m8 l8 xl8">
+                        <div class={`card lg-card lg-card-${idx}`} key={document.node.id} style={{top:"-4rem"}}>
+                          <Img fluid={document.node.image.childImageSharp.fluid} />
+                          <div class="card-action">
+                            <Link to={`/articles/${document.node.id}`}>{document.node.title}</Link>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )
+                } else {
+                  return (
+                    <div className="col s12 m4 l4 xl4">
+                      <div class="card " key={document.node.id}>
+                        <Img fluid={document.node.image.childImageSharp.fluid} />
+                        <div class="card-action">
+                          <Link to={`/articles/${document.node.id}`}>{document.node.title}</Link>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
             </div>
           </div>
-          <div className="col m3 l3 hide-on-small-only blue" style={{minHeight:600}}></div>
-          </div>        
-     </RowContainer>
+          <div className="col m3 l3 hide-on-small-only blue" style={{ minHeight: 600 }}></div>
+        </div>
+      </RowContainer>
     </Section>
   </Layout>
 )
