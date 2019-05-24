6a1f2e0c3a0422c1bae8d7466adf2421ed482cbb
chandni <chandnibpatel@gmail.com>
2019-05-24 08:34:49 -0500

    added Auth0 authentication

--- blog/src/components/navbar.js
+++ blog/src/components/navbar.js
@@ -3,11 +3,51 @@ import { Link } from "gatsby"
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import logo from "../assets/images/logo.svg" 
+import { logout, isAuthenticated, login,getProfile } from "../utils/auth"
 
 export class Navbar extends Component {
+    state = {
+          text: 'Login',
+          userName:'',
+          userNameDisplay:"none"
+    }
+//  userName="";
+
     componentDidMount() {
+    
         let sidenav = document.querySelectorAll(".sidenav");
         M.Sidenav.init(sidenav, {});
+        if (!isAuthenticated())
+         {
+            this.setState({
+              text: 'Login',
+              userName:'',
+              userNameDisplay:"none"
+            });
+           }
+          else
+          {
+            const user = getProfile()
+          
+            console.log(user);
+            this.setState({
+              text: 'Logout',
+              userName:user.name,
+              userNameDisplay:"inline"
+            });
+
+          }
+    }
+    loginClick = event =>{
+      event.preventDefault();
+      if (isAuthenticated()) {
+        window.location.href = "/login";
+        logout();
+      }
+      else{
+        login();
+        event.preventDefault();
+      }
     }
   render() {
     return (
@@ -20,13 +60,16 @@ export class Navbar extends Component {
          <Link to="/" className="brand-logo"><img style={{width: 150, marginLeft: 20}} src={logo} alt="ATXPC" /></Link>
           <a href="#!" data-target="slide-out" className="sidenav-trigger right"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
-            <li><Link to="/">Home</Link></li>
+            
             <li><Link to="/news">News</Link></li>
-            <li><Link to="/articles">Articles</Link></li>
+            <li><Link to="/articles">Articles</Link></li><li><Link to="/">Home</Link></li>
             <li><Link to="/events">Events</Link></li>
             <li><Link to="/resources">Resources</Link></li>
             <li><Link to="/services">Services</Link></li>
-            <li><a className="waves-effect waves-light btn pink accent-3 modal-trigger" href="#modal1">Login</a></li>
+           <li><label className="waves-effect waves-light btn pink accent-3" > {this.state.userName} </label></li>
+            
+            <li><a className="waves-effect waves-light btn pink accent-3" id="login-trigger" onClick={this.loginClick}>{this.state.text}</a></li>
+            
           </ul>
         </div>        
       </nav>
---
+++ blog/src/components/toggleBtn.js
@@ -0,0 +1,34 @@
+class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+      handleClick() {
+          this.setState(function(prevState) {
+              
+              return {isToggleOn: !prevState.isToggleOn};
+          });
+      }
+      
+      // ES6 -------
+      // handleClick() {
+      // 	this.setState(prevState => ({
+      // 		isToggleOn: !prevState.isToggleOn
+      // 	}));
+      // }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'Login' : 'Logout'}
+        </button>
+      );
+    }
+  }
+
+  export default Toggle;
+  
