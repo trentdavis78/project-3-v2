550e73b54617066dfbd04adc13e92ed5789a486b
Trent Davis <trentdavisinc@gmail.com>
2019-05-27 00:52:53 -0500

    Added social icons - Author details

--- blog/src/templates/author.js
+++ blog/src/templates/author.js
@@ -4,15 +4,18 @@ import Layout from '../components/layout'
 import Section from "../components/section"
 import RowContainer from "../components/rowContainer"
 import Img from 'gatsby-image'
-
-
-const UserTemplate = ({ data, pageContext }) => (
+import linkedin from "../images/icons/linkedin.svg" 
+import twitter from "../images/icons/twitter.svg" 
+import github from "../images/icons/github.svg" 
+import facebook from "../images/icons/facebook.svg" 
+import www from "../images/icons/www.svg"
+const UserTemplate = ({ data }) => (
 
     <Layout>
         <Section styleName={"bg-white"}>
             <RowContainer color={"white"}>
                 <div className="col s12 m6 l6 xl6">
-                    <div className="card">
+                    <div className="card author-card">
                         <div className="card-image waves-effect waves-block waves-light" 
                         style={{left:"50%",
                                 transform:"translateX(-50%)",
@@ -27,10 +30,47 @@ const UserTemplate = ({ data, pageContext }) => (
                             </span>
                             <p style={{padding:".33rem .2rem 1rem"}}>{data.details.title}</p>
                             <div class="card-action">
-                            <ul>
-                                <li>{data.details.linkedin}</li>
-                                <li>{data.details.twitter}</li>
-                                <li>{data.details.facebook}</li>
+                            <ul className="social-icons">
+                                <li>
+                                    { //Check if message failed
+                                        (data.details.linkedin === 'disabled')
+                                        ?  <img className="disabled" src={linkedin} style={{width:33,opacity:.5}} />
+                                        :  <a href={data.details.linkedin} alt="LinkedIn"><img src={linkedin} style={{width:33}} /></a> 
+                                    }
+                                   
+                                </li>
+                                <li>
+                                    { //Check if message failed
+                                        (data.details.github === 'disabled')
+                                        ?  <img className="disabled" src={github} style={{width:33,opacity:.5}} />
+                                        :  <a href={data.details.github} alt="Github"><img src={github} style={{width:33}} /></a> 
+                                    }
+                                   
+                                </li>
+                                <li>
+                                    { //Check if message failed
+                                        (data.details.twitter === 'disabled')
+                                        ?  <img className="disabled" src={twitter} style={{width:33,opacity:.5}} />
+                                        :  <a href={data.details.twitter} alt="Twitter"><img src={twitter} style={{width:33}} /></a> 
+                                    }
+                                   
+                                </li>
+                                <li>
+                                    { //Check if message failed
+                                        (data.details.facebook === 'disabled')
+                                        ?  <img className="disabled" src={facebook} style={{width:33,opacity:.5}} />
+                                        :  <a href={data.details.facebook} alt="Facebook"><img src={facebook} style={{width:33}} /></a> 
+                                    }
+                                   
+                                </li>
+                                <li>
+                                    { //Check if message failed
+                                        (data.details.www === 'disabled')
+                                        ?  <img className="disabled" src={www} style={{width:33,opacity:.5}} />
+                                        :  <a href={data.details.www} alt="Website"><img src={www} style={{width:33}} /></a> 
+                                    }
+                                   
+                                </li>
                             </ul>
                         </div>
                         </div>
@@ -95,6 +135,8 @@ export const query = graphql`
          linkedin
          facebook
          twitter
+         github
+         www
     }
   }
 ` 
