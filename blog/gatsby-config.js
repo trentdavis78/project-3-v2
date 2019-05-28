a7e409396ffba90a46ac3b26ab642296491c5102
Trent Davis <trentdavisinc@gmail.com>
2019-05-28 08:47:44 -0500

    gatsby-config.js --> Strapi Heroku

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: 'https://atxpc-cms.herokuapp.com',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'article',
           'user',
