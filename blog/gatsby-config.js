72bc9cc9f15aad4ed3b3031d5841866b54ef32ab
Penny <radiogaga66@gmail.com>
2019-05-29 21:15:36 -0500

    updated content and tag line

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'https://atxpc-cms.herokuapp.com',
+        apiURL: 'http://localhost:1337',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'article',
           'user',
