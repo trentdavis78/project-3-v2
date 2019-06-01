0742c02ed431dbdc1c6c5db34dcec036fb8b9821
Trent Davis <trentdavisinc@gmail.com>
2019-06-01 02:18:42 -0500

    Added web and logo design modals

--- src/pages/services.js
+++ src/pages/services.js
@@ -7,6 +7,8 @@ import RowContainer from "../components/rowContainer"
 import HorzCardSm from '../components/horzCardSm'
 import hwImg from "../images/hw.jpg"
 import swImg from "../images/sw.jpg"
+import webImg from "../images/web.jpg"
+import logoImg from "../images/logoDesign.jpg"
 import servicesImg from "../images/services.svg"
 
 
@@ -26,6 +28,14 @@ export class Services extends Component {
                 Computer and server repairs and maintenance. Services include building or configuring new hardware, installing and updating packages, and creating and maintaining computer networks.</HorzCardSm>
                <HorzCardSm image={swImg} title={"Software Repair"} id={"2"} modalId={"modalSw"}>
                Software repair services include diagnosis, data recovery, virus and spyware removal, software setup and installation, wi-fi setup or troubleshooting and more.
+               </HorzCardSm>
+              </div>
+              <div className="row">
+                <HorzCardSm image={webImg} title={"Website Design"} id={"3"} modalId={"modalWeb"}>
+                Our web designers can create the look, layout, and features of your website and work with our development teams and managers to keep your site up-to-date while prioritizing your needs.
+                </HorzCardSm>
+               <HorzCardSm image={logoImg} title={"Logo & Graphic Design"} id={"4"} modalId={"modalLogo"}>
+                Our graphic designers create visual concepts to communicate the ideas that inspire, inform, and captivate your consumers. We can develop the layout and design for advertisements, brochures, magazines, and corporate reports.
                </HorzCardSm>
               </div>
             </div>
