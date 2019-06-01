c761a5d1292fc88049fb8d2c0a2fc33f29413ba8
Trent Davis <trentdavisinc@gmail.com>
2019-05-31 22:59:07 -0500

    Favicon, SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
     )
   },
   siteMetadata: {
-    title: 'My super blog',
-    description: 'Gatsby blog with Strapi',
-    author: 'Strapi team'
+    title: 'ATXPC | Everything Austin Tech',
+    description: 'Austin technology hub for PC repair & Web Design freelancers, Tech Enthusiats & Bloggers, and everything in between.',
+    author: 'ATXPC team'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -54,10 +54,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#003594',
+        theme_color: '#003594',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
